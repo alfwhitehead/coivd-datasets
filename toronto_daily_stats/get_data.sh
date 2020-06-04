95f2c0c289476f7b7c100f0ea8b1643b11a0636e
Alfred Whitehead <awhitehead@klick.com>
2020-06-04 10:45:25 -0400

    Added daily pull of City of Toronto data

---
+++ toronto_daily_stats/get_data.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+torontofileid=1euhrML0rkV_hHF1thiA0G5vSSeZCqxHY
+dir=/Users/awhitehead/Code/covid-datasets/toronto_daily_stats
+today=`date +"%Y%m%d"`
+
+cd $dir
+date > run.log
+/usr/local/bin/wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1euhrML0rkV_hHF1thiA0G5vSSeZCqxHY' -O $today.csv  >run.log 2>&1
+git add $today.csv >run.log 2>&1
+git commit -m "Automated download of $today data" $today.csv >run.log 2>&1
+git push >run.log 2>&1
+
+
