9366466522de9f2302888b387e10f38274bd0e68
Alfred Whitehead <awhitehead@klick.com>
2020-06-04 10:48:05 -0400

    Bug fix: file extension

--- toronto_daily_stats/get_data.sh
+++ toronto_daily_stats/get_data.sh
@@ -6,7 +6,7 @@ today=`date +"%Y%m%d"`
 
 cd $dir
 date > run.log
-/usr/local/bin/wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1euhrML0rkV_hHF1thiA0G5vSSeZCqxHY' -O $today.csv  >run.log 2>&1
+/usr/local/bin/wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1euhrML0rkV_hHF1thiA0G5vSSeZCqxHY' -O $today.xlsx  >run.log 2>&1
 git add $today.csv >run.log 2>&1
 git commit -m "Automated download of $today data" $today.csv >run.log 2>&1
 git push >run.log 2>&1
